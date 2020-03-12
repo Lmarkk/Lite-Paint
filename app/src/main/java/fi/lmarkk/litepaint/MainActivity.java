d0c504c08187732180b06f49b4e6fd7ab4bc1922
Sorellum <lassi.markkinen@hotmail.com>
2020-03-12 15:31:19 +0200

    Instantiate PaintingView in MainActivity.

--- app/src/main/java/fi/lmarkk/litepaint/MainActivity.java
+++ app/src/main/java/fi/lmarkk/litepaint/MainActivity.java
@@ -1,14 +1,18 @@
 package fi.lmarkk.litepaint;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.LinearLayout;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+    private PaintingView paintingView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        paintingView= (PaintingView) findViewById(R.id.painting_view);
     }
 }
