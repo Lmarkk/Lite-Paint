622bb70a618db8fef0d3c3de6834a8faf3009f56
Sorellum <lassi.markkinen@hotmail.com>
2020-03-12 15:35:37 +0200

    Set initial paint color.

--- app/src/main/java/fi/lmarkk/litepaint/MainActivity.java
+++ app/src/main/java/fi/lmarkk/litepaint/MainActivity.java
@@ -8,11 +8,15 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
     private PaintingView paintingView;
+    private ImageButton currentPaint;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         paintingView= (PaintingView) findViewById(R.id.painting_view);
+        LinearLayout paintLayout = (LinearLayout)findViewById(R.id.paint_colors);
+        currentPaint = (ImageButton)paintLayout.getChildAt(0);
+        currentPaint.setImageDrawable(getResources().getDrawable(R.drawable.paint_pressed));
     }
 }
