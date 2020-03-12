b0282c0f64e20e68b9bad3bd987a29f9215c3683
Sorellum <lassi.markkinen@hotmail.com>
2020-03-12 15:13:23 +0200

    Initialize variables for PaintingView.

--- app/src/main/java/fi/lmarkk/litepaint/PaintingView.java
+++ app/src/main/java/fi/lmarkk/litepaint/PaintingView.java
@@ -3,9 +3,21 @@ package fi.lmarkk.litepaint;
 import android.content.Context;
 import android.util.AttributeSet;
 import android.view.View;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.graphics.Path;
+import android.view.MotionEvent;
 
 public class PaintingView extends View {
+    private Path drawPath;
+    private Paint drawPaint, canvasPaint;
+    private int paintColor = 0xFF660000;
+    private Canvas drawCanvas;
+    private Bitmap canvasBitmap;
+
     public PaintingView(Context context, AttributeSet attrs){
         super(context, attrs);
     }
+
 }
