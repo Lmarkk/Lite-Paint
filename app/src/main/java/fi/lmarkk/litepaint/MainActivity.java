283594623d7ee5156eb50fe836aecb3c5d143c7c
Sorellum <lassi.markkinen@hotmail.com>
2020-03-16 14:49:40 +0200

    Implement OnClickListener in MainActivity. Create brush size Dialog in onClick method.

--- app/src/main/java/fi/lmarkk/litepaint/MainActivity.java
+++ app/src/main/java/fi/lmarkk/litepaint/MainActivity.java
@@ -1,12 +1,14 @@
 package fi.lmarkk.litepaint;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Dialog;
 import android.view.View;
 import android.widget.ImageButton;
 import android.widget.LinearLayout;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
     private PaintingView paintingView;
     private ImageButton currentPaint,  drawButton;
     private float smallBrush, mediumBrush, largeBrush;
@@ -25,7 +27,7 @@ public class MainActivity extends AppCompatActivity {
         drawButton = (ImageButton)findViewById(R.id.draw_button);
     }
 
-    public void paintClicked(View view){
+    public void paintClicked(View view) {
         if(view != currentPaint){
             ImageButton imageView = (ImageButton)view;
             String color = view.getTag().toString();
@@ -33,6 +35,14 @@ public class MainActivity extends AppCompatActivity {
             imageView.setImageDrawable(getResources().getDrawable(R.drawable.paint_pressed));
             currentPaint.setImageDrawable(getResources().getDrawable(R.drawable.paint));
             currentPaint=(ImageButton)view;
+        }
+    }
+
+    @Override
+    public void onClick(View view) {
+        if(view.getId() == R.id.draw_button) {
+            final Dialog brushDialog = new Dialog(this);
+            brushDialog.setTitle("Brush size:");
         }
     }
 }
