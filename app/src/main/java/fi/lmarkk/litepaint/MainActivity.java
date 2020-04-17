17f3d6bbd9b746cdd9497e33016a669d5d63ad48
Sorellum <lassi.markkinen@hotmail.com>
2020-04-17 13:07:19 +0300

    Merge develop into master.

--- app/src/main/java/fi/lmarkk/litepaint/MainActivity.java
+++ app/src/main/java/fi/lmarkk/litepaint/MainActivity.java
@@ -1,47 +1,53 @@
 package fi.lmarkk.litepaint;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
+import android.app.AlertDialog;
 import android.app.Dialog;
+import android.content.DialogInterface;
+import android.content.pm.PackageManager;
+import android.provider.MediaStore;
 import android.view.View;
 import android.widget.ImageButton;
 import android.widget.LinearLayout;
 import android.os.Bundle;
+import android.widget.Toast;
+
+import com.flask.colorpicker.ColorPickerView;
+import com.flask.colorpicker.OnColorSelectedListener;
+import com.flask.colorpicker.builder.ColorPickerClickListener;
+import com.flask.colorpicker.builder.ColorPickerDialogBuilder;
+
+import java.util.UUID;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    static final int MY_PERMISSIONS_REQUEST_WRITE_EXTERNAL_STORAGE = 1;
     private PaintingView paintingView;
-    private ImageButton currentPaint,  drawButton, eraseButton;
+    private ImageButton drawButton, eraseButton, newButton, saveButton, colorPickerButton;
     private float smallBrush, mediumBrush, largeBrush;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        paintingView= (PaintingView) findViewById(R.id.painting_view);
-        LinearLayout paintLayout = (LinearLayout)findViewById(R.id.paint_colors);
-        currentPaint = (ImageButton)paintLayout.getChildAt(0);
-        currentPaint.setImageDrawable(getResources().getDrawable(R.drawable.paint_pressed));
+        paintingView= findViewById(R.id.painting_view);
         smallBrush = getResources().getInteger(R.integer.small_size);
         mediumBrush = getResources().getInteger(R.integer.medium_size);
         largeBrush = getResources().getInteger(R.integer.large_size);
-        drawButton = (ImageButton)findViewById(R.id.draw_button);
+        newButton = findViewById(R.id.new_drawing_button);
+        newButton.setOnClickListener(this);
+        drawButton = findViewById(R.id.draw_button);
         drawButton.setOnClickListener(this);
-        eraseButton = (ImageButton)findViewById(R.id.erase_button);
+        eraseButton = findViewById(R.id.erase_button);
         eraseButton.setOnClickListener(this);
+        saveButton = findViewById(R.id.save_button);
+        saveButton.setOnClickListener(this);
+        colorPickerButton = findViewById(R.id.color_picker_button);
+        colorPickerButton.setOnClickListener(this);
         paintingView.setBrushSize(mediumBrush);
-    }
-
-    public void paintClicked(View view) {
-        paintingView.setErase(false);
-        paintingView.setBrushSize(paintingView.getLastBrushSize());
-        if(view != currentPaint){
-            ImageButton imageView = (ImageButton)view;
-            String color = view.getTag().toString();
-            paintingView.setColor(color);
-            imageView.setImageDrawable(getResources().getDrawable(R.drawable.paint_pressed));
-            currentPaint.setImageDrawable(getResources().getDrawable(R.drawable.paint));
-            currentPaint=(ImageButton)view;
-        }
     }
 
     @Override
@@ -73,7 +79,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 }
             });
 
-            ImageButton largeButton = (ImageButton)brushDialog.findViewById(R.id.large_brush);
+            ImageButton largeButton = brushDialog.findViewById(R.id.large_brush);
             largeButton.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
@@ -90,7 +96,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             brushDialog.setTitle("Eraser size:");
             brushDialog.setContentView(R.layout.brush_chooser);
 
-            ImageButton smallBtn = (ImageButton)brushDialog.findViewById(R.id.small_brush);
+            ImageButton smallBtn = brushDialog.findViewById(R.id.small_brush);
             smallBtn.setOnClickListener(new View.OnClickListener(){
                 @Override
                 public void onClick(View v) {
@@ -100,7 +106,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 }
             });
 
-            ImageButton mediumBtn = (ImageButton)brushDialog.findViewById(R.id.medium_brush);
+            ImageButton mediumBtn = brushDialog.findViewById(R.id.medium_brush);
             mediumBtn.setOnClickListener(new View.OnClickListener(){
                 @Override
                 public void onClick(View v) {
@@ -110,7 +116,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 }
             });
 
-            ImageButton largeBtn = (ImageButton)brushDialog.findViewById(R.id.large_brush);
+            ImageButton largeBtn = brushDialog.findViewById(R.id.large_brush);
             largeBtn.setOnClickListener(new View.OnClickListener(){
                 @Override
                 public void onClick(View v) {
@@ -121,6 +127,109 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             });
 
             brushDialog.show();
+        } else if(view.getId() == R.id.new_drawing_button) {
+            AlertDialog.Builder newDialog = new AlertDialog.Builder(this);
+            newDialog.setTitle("New drawing");
+            newDialog.setMessage("Start new drawing (you will lose the current drawing)?");
+            newDialog.setPositiveButton("Yes", new DialogInterface.OnClickListener(){
+                public void onClick(DialogInterface dialog, int which){
+                    paintingView.startNew();
+                    dialog.dismiss();
+                }
+            });
+            newDialog.setNegativeButton("Cancel", new DialogInterface.OnClickListener(){
+                public void onClick(DialogInterface dialog, int which){
+                    dialog.cancel();
+                }
+            });
+            newDialog.show();
+        } else if(view.getId() == R.id.save_button) {
+            if(ContextCompat.checkSelfPermission(this,
+                    Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                    != PackageManager.PERMISSION_GRANTED) {
+
+                ActivityCompat.requestPermissions(this,
+                        new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE},
+                        MY_PERMISSIONS_REQUEST_WRITE_EXTERNAL_STORAGE);
+            } else {
+                makeSaveDialog();
+            }
+        } else if(view.getId() == R.id.color_picker_button) {
+            paintingView.setErase(false);
+            paintingView.setBrushSize(paintingView.getLastBrushSize());
+            showColorPickerDialog(view);
         }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        switch (requestCode) {
+            case MY_PERMISSIONS_REQUEST_WRITE_EXTERNAL_STORAGE: {
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    makeSaveDialog();
+                }
+                return;
+            }
+        }
+    }
+
+    private void makeSaveDialog() {
+        if(ContextCompat.checkSelfPermission(this,
+                Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                == PackageManager.PERMISSION_GRANTED) {
+            AlertDialog.Builder saveDialog = new AlertDialog.Builder(this);
+            saveDialog.setTitle("Save drawing");
+            saveDialog.setMessage("Save drawing to device Gallery?");
+            saveDialog.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                public void onClick(DialogInterface dialog, int which){
+                    paintingView.setDrawingCacheEnabled(true);
+                    String imgSaved = MediaStore.Images.Media.insertImage(
+                            getContentResolver(), paintingView.getDrawingCache(),
+                            UUID.randomUUID().toString()+".png", "drawing");
+                    if(imgSaved!=null){
+                        Toast savedToast = Toast.makeText(getApplicationContext(),
+                                "Drawing saved to Gallery!", Toast.LENGTH_SHORT);
+                        savedToast.show();
+                    }
+                    else{
+                        Toast unsavedToast = Toast.makeText(getApplicationContext(),
+                                "Oops! Image could not be saved.", Toast.LENGTH_SHORT);
+                        unsavedToast.show();
+                    }
+                    paintingView.destroyDrawingCache();
+                }
+            });
+            saveDialog.setNegativeButton("Cancel", new DialogInterface.OnClickListener(){
+                public void onClick(DialogInterface dialog, int which){
+                    dialog.cancel();
+                }
+            });
+            saveDialog.show();
+        }
+    }
+
+    public void showColorPickerDialog(View v) {
+        ColorPickerDialogBuilder
+                .with(this)
+                .setTitle("Choose color")
+                .initialColor(paintingView.getPaintColor())
+                .wheelType(ColorPickerView.WHEEL_TYPE.FLOWER)
+                .density(12)
+                .setOnColorSelectedListener(new OnColorSelectedListener() {
+                    @Override
+                    public void onColorSelected(int selectedColor) {}
+                })
+                .setPositiveButton("ok", new ColorPickerClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int selectedColor, Integer[] allColors) {
+                        paintingView.setColor("#" + Integer.toHexString(selectedColor).toUpperCase());
+                    }
+                })
+                .setNegativeButton("cancel", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {}
+                })
+                .build()
+                .show();
     }
 }
